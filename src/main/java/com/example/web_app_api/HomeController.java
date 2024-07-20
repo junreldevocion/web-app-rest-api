3fa003e6e256ca35bc33b3426d87dc0eb33edd85
Junrel Devocion <junrel.devocion.-nd@disney.com>
2024-07-20 13:45:19 +0800

    first commit

---
+++ src/main/java/com/example/web_app_api/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.web_app_api;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String greeting() {
+        return "hello world";
+    }
+
+    @GetMapping("/{id}")
+    public String hello(@PathVariable int id) {
+        return "bwesit" + id;
+    }
+
+}
